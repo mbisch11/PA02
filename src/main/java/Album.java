38062fa817c6a43a12b1daf7da59f77145241363
mbisch11 <mbisch11@gmail.com>
2025-03-23 16:25:21 -0400

    Create instance variables and constructors for Album Classs

--- src/main/java/Album.java
+++ src/main/java/Album.java
@@ -1,5 +1,24 @@
-public class Album implements Comparable{
+import java.util.ArrayList;
 
+public class Album implements Comparable{
+    private int idNum;
+    private ArrayList<String> artists;
+    private String title;
+    private int numSongs;
+
+    public Album(int idNum, ArrayList<String> artists, String title, int numSongs){
+        this.idNum = idNum;
+        this.artists = artists;
+        this.title = title;
+        this.numSongs = numSongs;
+    }
+
+    public Album(int idNum, String title, int numSongs){
+        this.idNum = idNum;
+        this.artists = new ArrayList<>();
+        this.title = title;
+        this.numSongs = numSongs;
+    }
 
     @Override
     public int compareTo(Object o) {
